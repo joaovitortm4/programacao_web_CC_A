1de6c2688c63c9f57460c8461209aae63fdce5d4
joaovitortm4 <joao.marra@sempreceub.com>
2024-08-12 10:52:19 -0300

    adicionando js

---
+++ script.js
@@ -0,0 +1,13 @@
+let botaoSubmeter = document.getElementById("botão submeter");
+
+botaoSubmeter.addEventListener("click",submeter)
+
+
+function submeter(){
+    let elementoNome = document.getElementById("nome").value;
+    let elementoIdade = document.getElementById("idade").value;
+    let elementoEmail = document.getElementById("email").value;
+    console.log(elementoNome);
+    console.log(elementoIdade);
+    console.log(elementoEmail);
+}
