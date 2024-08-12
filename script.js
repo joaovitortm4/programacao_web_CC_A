a130770f5e78a4bdb620a7b169934c8fe24669b4
joaovitortm4 <joao.marra@sempreceub.com>
2024-08-12 11:08:30 -0300

    validando email

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-let botaoSubmeter = document.getElementById("botão submeter");
+let botaoSubmeter = document.getElementById("botão-submeter");
 
-botaoSubmeter.addEventListener("click",submeter)
+botaoSubmeter.addEventListener("click", submeter);
 
-
-function submeter(){
+function submeter() {
     let elementoNome = document.getElementById("nome").value;
     let elementoIdade = document.getElementById("idade").value;
     let elementoEmail = document.getElementById("email").value;
+
     console.log(elementoNome);
     console.log(elementoIdade);
     console.log(elementoEmail);
+
+    if (validarEmail(elementoEmail)) {
+        alert("Email válido!");
+    } else {
+        alert("Email inválido!");
+    }
+}
+
+function validarEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
 }
